bcff30e0f04e379b9d2a65bc8755a5b555a0e812
dannyx <daro322.dp@gmail.com>
2025-12-09 21:19:16 +0100

    add plugins and more stuff with engine

---
+++ plugins/notifications/notify.py
@@ -0,0 +1,29 @@
+import platform
+
+def register(engine):
+    system = platform.system()
+
+    icon_path = None
+
+    if system == "Darwin":
+        import pync
+        def notify(title, message):
+            pync.notify(
+                message,
+                title=title,
+                appIcon=icon_path,
+                activate="com.apple.Terminal"
+            )
+    else:
+        from plyer import notification
+        def notify(title, message):
+            notification.notify(
+                title=title,
+                message=message,
+                app_name="PyPhoton",
+                app_icon=icon_path,
+                timeout=5
+            )
+
+    engine.notifications = notify
+    print("[Notifications] Plugin ready")
